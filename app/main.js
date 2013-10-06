71f7f88e9e709e8e41b0bc16c3c6bedf43aaa344
Sahat Yalkabov <sakhat@gmail.com>
2013-10-06 12:45:11 -0400

    Merged config and main

--- app/main.js
+++ app/main.js
@@ -1,14 +1,43 @@
-// Break out the application running from the configuration definition to
-// assist with testing.
-require(["config"], function() {
-  // Kick off the application.
-  require(["app", "router"], function(app, Router) {
-    // Define your master router on the application namespace and trigger all
-    // navigation from this instance.
-    app.router = new Router();
+// This is the runtime configuration file.  It complements the Gruntfile.js by
+// supplementing shared properties.
+require.config({
+  paths: {
+    // Make vendor easier to access.
+    "vendor": "../vendor",
 
-    // Trigger the initial route and enable HTML5 History API support, set the
-    // root folder to '/' by default.  Change in app.js.
-    Backbone.history.start({ pushState: true, root: app.root });
-  });
+    // Almond is used to lighten the output filesize.
+    "almond": "../vendor/bower/almond/almond",
+
+    "text": "../vendor/bower/requirejs-text/text",
+
+    // Opt for Lo-Dash Underscore compatibility build over Underscore.
+    "underscore": "../vendor/bower/lodash/dist/lodash.underscore",
+
+    // Map remaining vendor dependencies.
+    "jquery": "../vendor/bower/jquery/jquery",
+    "backbone": "../vendor/bower/backbone/backbone"
+  },
+
+  shim: {
+    // This is required to ensure Backbone works as expected within the AMD
+    // environment.
+    "backbone": {
+      // These are the two hard dependencies that will be loaded first.
+      deps: ["jquery", "underscore"],
+
+      // This maps the global `Backbone` object to `require("backbone")`.
+      exports: "Backbone"
+    }
+  }
+});
+
+// Kick off the application.
+require(["app", "router"], function(app, Router) {
+  // Define your master router on the application namespace and trigger all
+  // navigation from this instance.
+  app.router = new Router();
+
+  // Trigger the initial route and enable HTML5 History API support, set the
+  // root folder to '/' by default.  Change in app.js.
+  Backbone.history.start({ pushState: true, root: app.root });
 });
